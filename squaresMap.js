86d2f764c3cccc836d5a8a66d3f184a98e16ce5d
biancavee15 <vasquezbmarie@gmail.com>
2018-02-20 15:14:53 -0500

    added copying drills and squares map

---
+++ squaresMap.js
@@ -0,0 +1,67 @@
+/* Write a function called squares that takes a single argument: 
+an array of values called array. 
+The function should return a new array with the square of each value from the original array. 
+So if the input array was [1, 2, 9, 10], the output should be [1, 4, 81, 100]. 
+You should not use a loop, which will be covered in the next reading. 
+You should use the .map method, which we discussed in the previous assignment.*/
+
+
+function squares(array) {
+    return array.map(num => num *num);
+}
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+  // tests
+  
+  function testFunctionWorks(fn, input, expected) {
+    const result = fn(input);
+    if (
+      result &&
+      result.length === expected.length &&
+      result.every(function(item) {
+        return expected.indexOf(item) > -1;
+      })
+    ) {
+      console.log('SUCCESS: `' + fn.name + '` works!');
+      return true;
+    } else {
+      console.error(
+        'FAILURE: `' +
+          fn.name +
+          '([' +
+          input +
+          '])` should be ' +
+          expected +
+          ' but was ' +
+          fn(input)
+      );
+      return false;
+    }
+  }
+  
+  function runTests() {
+    const input1 = [1, 2, 3, 4, 5];
+    const result1 = [1, 4, 9, 16, 25];
+    const input2 = [2, 4, 6, 8];
+    const result2 = [4, 16, 36, 64];
+  
+    const testResults = [
+      testFunctionWorks(squares, input1, result1),
+      testFunctionWorks(squares, input2, result2),
+    ];
+  
+    const numPassing = testResults.filter(function(result) {
+      return result;
+    }).length;
+    console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
+  }
+  
+  runTests();
+  
