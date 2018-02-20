e8e42e779389523e2bea76c1521cfaf325bd8942
biancavee15 <vasquezbmarie@gmail.com>
2018-02-20 12:41:22 -0500

    finished creatingArraysDrill.js

--- creatingArraysDrill.js
+++ creatingArraysDrill.js
@@ -1,7 +1,7 @@
 function makeList(item1, item2, item3) {
-    // your code here
+    return [item1, item2, item3];
   }
-  
+ 
   /* From here down, you are not expected to 
      understand.... for now :)  
      
@@ -12,22 +12,23 @@ function makeList(item1, item2, item3) {
   
   // tests
   
-  function testMakeList() {
-    const items = ['prime rib', 'fried goat cheese salad', 'fish tacos'];
-    const result = makeList(items[0], items[1], items[2]);
-  
-    if (
-      result &&
-      result.length &&
-      items.length === result.length &&
-      items.every(function(item) {
-        return result.indexOf(item) > -1;
-      })
-    ) {
-      console.log('SUCCESS: `makeList` works!');
-    } else {
-      console.error('FAILURE: `makeList` is not working');
+    function testMakeList() {
+        const items = ['prime rib', 'fried goat cheese salad', 'fish tacos'];
+        const result = makeList(items[0], items[1], items[2]);
+    
+        if (
+        result &&
+        result.length &&
+        items.length === result.length &&
+        items.every(function(item) {
+            return result.indexOf(item) > -1;
+        })
+        ) {
+        console.log('SUCCESS: `makeList` works!');
+        } else {
+        console.error('FAILURE: `makeList` is not working');
+        }
     }
-  }
-  
-  testMakeList();
+    
+    testMakeList();
+    
