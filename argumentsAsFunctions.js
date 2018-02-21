d0d025bcfde3e09928c41a396478bc9f896adda4
biancavee15 <vasquezbmarie@gmail.com>
2018-02-20 21:11:43 -0500

    changed argument as function around

--- argumentsAsFunctions.js
+++ argumentsAsFunctions.js
@@ -4,7 +4,7 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
 const filteredNames = filter(myNames, function(name) {
     // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'J';
+    return name[0] === 'R';
 });
 
 console.log(filteredNames) // => ['Rich', 'Ray']
